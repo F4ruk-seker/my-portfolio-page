a565a050970dfa908cbc4b53392e2e4727abe2f0
F4ruk.seker <ahmetfaruk731@gmail.com>
2023-08-20 08:20:31 +0300

    change : RoadMAP model slug editable disable

--- Roadmap/models.py
+++ Roadmap/models.py
@@ -9,7 +9,6 @@ class RoadMapModel(models.Model):
     slug = AutoSlugField(
         populate_from='name',  # Use 'name' field to populate the slug
         slugify=slugify,  # Use the slugify function from Django's utils
-        editable=True
     )
     created = models.DateField(auto_now_add=True)  # Use auto_now_add for creation date
     can_share = models.BooleanField(default=False)
