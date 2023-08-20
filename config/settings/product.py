efcf9b6899bdb9aa4f920d45ed27316df52c68e2
F4ruk.seker <ahmetfaruk731@gmail.com>
2023-08-20 18:48:25 +0300

    delete sentry

--- config/settings/product.py
+++ config/settings/product.py
@@ -55,23 +55,6 @@ LOGGING = {
     },
 }
 
-import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
-
-sentry_sdk.init(
-    dsn=env("SENTRY_DSN"),
-    integrations=[DjangoIntegration(),],
-
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production.
-    traces_sample_rate=1.0,
-
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True
-)
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -90,7 +73,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
